413b108d6b097673c0e5ecedd340f4b74c871178
Meowveloper <meowveloper.dev@gmail.com>
2025-02-08 23:33:42 +0700

    style fix

---
+++ src/scripts/login.js
@@ -0,0 +1,8 @@
+const login_form = document.getElementById("login_form");
+
+login_form.addEventListener("submit", (e) => {
+    e.preventDefault(); 
+    // const api_url = import.meta.env.API_URL;
+    // console.log(api_url);
+    console.log("login");
+})
